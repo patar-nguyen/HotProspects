49cf9c6ebdc3934459ed77b9307731d6bf4274cb
Patrick Nguyen <patar.nguyen@gmail.com>
2023-09-28 17:22:31 -0400

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Patrick Nguyen on 2023-09-28.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
